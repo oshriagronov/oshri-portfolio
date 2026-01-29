f26de0018e21ee340c1c3cbf1c930f0fdb2bda5a
oshriagronov <fineness-uncut-tag@duck.com>
2026-01-29 11:04:02 +0200

    moved replace project and about

--- src/data.jsx
+++ src/data.jsx
@@ -1,7 +1,7 @@
 import { nanoid } from "nanoid";
 export const links = [
   { id: nanoid(), href: "#home", text: "home" },
-  { id: nanoid(), href: "#about", text: "about" },
   { id: nanoid(), href: "#projects", text: "projects" },
+  { id: nanoid(), href: "#about", text: "about" },
   { id: nanoid(), href: "#contact", text: "contact" },
 ];
